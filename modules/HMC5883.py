8bde1d7b2b59e3be401bdb4fb3cd1bb42ebc5d17
Max <sonthaya.nt@hotmail.com>
2021-01-18 21:34:39 +0700

    Add support IPST-WiFi

--- modules/HMC5883.py
+++ modules/HMC5883.py
@@ -2,10 +2,15 @@ from machine import Pin
 from machine import I2C
 from time import sleep
 import math
+import os
 
 HMC5883_ADDR = 0x1E
 
-i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+machine = os.uname().machine
+if "KidBright32" in machine:
+    i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+else:
+    i2c1 = I2C(0, scl=Pin(22), sda=Pin(21), freq=100000)
 
 def setup():
     i2c1.writeto_mem(HMC5883_ADDR, 0x02, b'\x00')
