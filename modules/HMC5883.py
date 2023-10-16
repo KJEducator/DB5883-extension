37ab1a20b2d84292c47218e26d15aee9b6b84712
KJEducator <kirati.educator@gmail.com>
2023-10-16 18:44:09 +0700

    Update HMC5883.py

--- modules/HMC5883.py
+++ modules/HMC5883.py
@@ -12,17 +12,20 @@ i2c1 = SoftI2C(scl=Pin(5), sda=Pin(4), freq=100_000)
 
 def setup():
     #i2c1.writeto_mem(HMC5883_ADDR, 0x02, b'\x00') ##Set (0x02 Register) mode to Continuous (00)
-    i2c1.writeto_mem(HMC5883_ADDR, DB5883_SETUP_ADDR, b'\x00')
+    i2c1.writeto_mem(HMC5883_ADDR, DB5883_SETUP_ADDR, b'\x1d')
 
 def b2i(x, y):
     return (x << 8 | y) if not x & 0x80 else (-(((x ^ 255) << 8) | (y ^ 255) + 1))
 
 def raw():
     #d = i2c1.readfrom_mem(HMC5883_ADDR, 0x03, 6)
-    d = i2c1.readfrom_mem(HMC5883_ADDR, DB5883_READ_ADDR, 6)
-    x = b2i(d[0], d[1]) * 0.92 * 0.1 # mG to uT
-    y = b2i(d[2], d[3]) * 0.92 * 0.1
-    z = b2i(d[4], d[5]) * 0.92 * 0.1 
+    d = i2c1.readfrom_mem(HMC5883_ADDR, 0x00, 6)
+    #x = b2i(d[0], d[1]) * 0.92 * 0.1 # mG to uT
+    #y = b2i(d[2], d[3]) * 0.92 * 0.1
+    #z = b2i(d[4], d[5]) * 0.92 * 0.1 
+    x = b2i(d[1], d[0]) * 0.92 * 0.1 # mG to uT
+    y = b2i(d[3], d[2]) * 0.92 * 0.1
+    z = b2i(d[5], d[4]) * 0.92 * 0.1 
     s = math.sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
     return (x, y, z, s)
 
@@ -38,8 +41,6 @@ def heading():
     if heading > 2 * math.pi:
         heading -= 2 * math.pi
     return heading * 180 / math.pi
-    #d = i2c1.readfrom_mem(HMC5883_ADDR, 0x0d, 8)
-    return d[7]
 
 def calibrate():
     global xMax, xMin, yMax, yMin
