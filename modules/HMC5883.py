c9004a346ca22fc233f20341dee459da92233369
KJEducator <kirati.educator@gmail.com>
2023-10-16 17:30:19 +0700

    Update HMC5883.py

--- modules/HMC5883.py
+++ modules/HMC5883.py
@@ -11,15 +11,15 @@ DB5883_READ_ADDR = 0x00
 i2c1 = SoftI2C(scl=Pin(5), sda=Pin(4), freq=100_000)
 
 def setup():
-    i2c1.writeto_mem(HMC5883_ADDR, 0x02, b'\x00') ##Set (0x02 Register) mode to Continuous (00)
-    #i2c1.writeto_mem(HMC5883_ADDR, DB5883_SETUP_ADDR, b'\x00')
+    #i2c1.writeto_mem(HMC5883_ADDR, 0x02, b'\x00') ##Set (0x02 Register) mode to Continuous (00)
+    i2c1.writeto_mem(HMC5883_ADDR, DB5883_SETUP_ADDR, b'\x00')
 
 def b2i(x, y):
     return (x << 8 | y) if not x & 0x80 else (-(((x ^ 255) << 8) | (y ^ 255) + 1))
 
 def raw():
-    d = i2c1.readfrom_mem(HMC5883_ADDR, 0x03, 6)
-    #d = i2c1.readfrom_mem(HMC5883_ADDR, DB5883_READ_ADDR, 6)
+    #d = i2c1.readfrom_mem(HMC5883_ADDR, 0x03, 6)
+    d = i2c1.readfrom_mem(HMC5883_ADDR, DB5883_READ_ADDR, 6)
     x = b2i(d[0], d[1]) * 0.92 * 0.1 # mG to uT
     y = b2i(d[2], d[3]) * 0.92 * 0.1
     z = b2i(d[4], d[5]) * 0.92 * 0.1 
